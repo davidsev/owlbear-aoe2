79e167f06830c82d64dfe41dd3ba236b71eaa618
David Severwright <code@davidsev.co.uk>
2024-04-21 23:47:08 +0100

    fixed the style of the dropdowns

--- src/UI/Components/SelectEnum.ts
+++ src/UI/Components/SelectEnum.ts
@@ -8,6 +8,10 @@ export class SelectEnum<T extends Record<string, string>> extends BaseElement {
     constructor (options: T) {
         super();
         this.options = options;
+
+        // Make sure we have a tabindex so onblur works
+        if (this.tabIndex < 0)
+            this.tabIndex = 0;
     }
 
     // Declare reactive properties
@@ -15,25 +19,44 @@ export class SelectEnum<T extends Record<string, string>> extends BaseElement {
     accessor options: T;
     @property()
     accessor value: keyof T = '';
+    @property()
+    accessor active: boolean = false;
 
-    @query('select')
-    accessor select!: HTMLSelectElement;
+    @query('div#input')
+    private accessor input!: HTMLDivElement;
+    @query('div#dropdown')
+    private accessor dropdown!: HTMLDivElement;
 
     // Render the UI as a function of component state
     render () {
         return html`
-            <select @change="${this.changeHandler}">
-                ${Object.entries(this.options).map(([value, label]) => html`
-                    <option value="${value}" ?selected="${value === this.value}">
-                        ${label}
-                    </option>
-                `)}
-            </select>
+            <div class="relative">
+                <div id="input" class="input ${this.active ? 'active' : ''}" @click="${this.toggleDropdown}">
+                    ${this.options[this.value]}
+                </div>
+                <div id="dropdown" class="dropdown ${this.active ? '' : 'hidden'}">
+                    ${Object.entries(this.options).map(([value, label]) => html`
+                        <div @click="${this.selectItem.bind(this, value)}"
+                             class="item ${value === this.value ? 'active' : ''}">
+                            ${label}
+                        </div>
+                    `)}
+                </div>
+            </div>
         `;
     }
 
-    private changeHandler () {
-        this.value = this.select?.value || '';
+    connectedCallback () {
+        super.connectedCallback();
+        this.addEventListener('blur', () => this.active = false);
+    }
+
+    private toggleDropdown () {
+        this.active = !this.active;
+    }
+
+    private selectItem (item: string) {
+        this.value = item;
         this.dispatchEvent(new Event('change'));
     }
 }
