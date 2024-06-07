3fd7d50b7f0a3b9ed5eba0dd5b790a565e7655af
David Severwright <code@davidsev.co.uk>
2024-06-07 22:21:05 +0100

    fixed the hex cube using the square settings

--- src/Tool/CubeTool.ts
+++ src/Tool/CubeTool.ts
@@ -1,9 +1,10 @@
 import { BaseTool } from './BaseTool';
 import { getId } from '../Utils/getId';
 import { CubeTemplateShape } from '../Shape/CubeTemplateShape';
-import { roomMetadata, SquareCubeStyle } from '../Metadata/room';
+import { roomMetadata, SquareCubeStyle, SquareDirection } from '../Metadata/room';
 import { BaseShape } from '../Shape/BaseShape';
 import { CubeSimpleShape } from '../Shape/CubeSimpleShape';
+import { grid } from '../../../owlbear-utils/src';
 
 export class CubeTool extends BaseTool {
 
@@ -12,15 +13,24 @@ export class CubeTool extends BaseTool {
     readonly id = getId('cube');
 
     protected getShape (): BaseShape {
-        if (roomMetadata.data.squareCubeStyle == SquareCubeStyle.SQUARE) {
-            return new CubeSimpleShape();
-        } else { // TEMPLATE
+        if (grid.type == 'HEX_HORIZONTAL' || grid.type == 'HEX_VERTICAL') {
             return new CubeTemplateShape(
-                roomMetadata.data.squareCubeStartPoints,
-                roomMetadata.data.squareCubeOverlapThreshold,
-                roomMetadata.data.squareCubeSizeSnapping,
-                roomMetadata.data.squareCubeDirection,
+                roomMetadata.data.hexCubeStartPoints,
+                roomMetadata.data.hexCubeOverlapThreshold,
+                roomMetadata.data.hexCubeSizeSnapping,
+                SquareDirection.ALL,
             );
+        } else {
+            if (roomMetadata.data.squareCubeStyle == SquareCubeStyle.SQUARE) {
+                return new CubeSimpleShape();
+            } else { // TEMPLATE
+                return new CubeTemplateShape(
+                    roomMetadata.data.squareCubeStartPoints,
+                    roomMetadata.data.squareCubeOverlapThreshold,
+                    roomMetadata.data.squareCubeSizeSnapping,
+                    roomMetadata.data.squareCubeDirection,
+                );
+            }
         }
     }
 }
