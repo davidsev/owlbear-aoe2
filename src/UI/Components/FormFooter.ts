2a21d255479f37c5873055a24aaded84ca1db164
David Severwright <code@davidsev.co.uk>
2024-07-07 22:36:32 +0100

    added the "Delete All" link, and cleaned up the form footer

--- src/UI/Components/FormFooter.ts
+++ src/UI/Components/FormFooter.ts
@@ -3,6 +3,8 @@ import { customElement } from 'lit/decorators.js';
 import { BaseElement } from '../BaseElement';
 import style from './FormFooter.css';
 import { baseCSS } from '../baseCSS';
+import OBR, { Item } from '@owlbear-rodeo/sdk';
+import { getId } from '../../Utils/getId';
 
 @customElement('form-footer')
 export class FormFooter extends BaseElement {
@@ -13,10 +15,16 @@ export class FormFooter extends BaseElement {
     render () {
         return html`
             <main>
-                <span>v${VERSION}</span>
+                <a href="https://github.com/davidsev/owlbear-aoe2" target="_blank">AoE Shapes v${VERSION}</a>
                 &nbsp;|&nbsp;
-                <a href="https://github.com/davidsev/owlbear-aoe2" target="_blank">GitHub</a>
+                <a @click="${this.deleteAll}">Delete All Shapes</a>
             </main>
         `;
+    }
+
+    async deleteAll () {
+        const itemsToDelete = await OBR.scene.items.getItems((item: Item) => item.metadata?.createdBy === getId());
+        const idsToDelete = itemsToDelete.map((item: Item) => item.id);
+        await OBR.scene.items.deleteItems(idsToDelete);
     }
 }
