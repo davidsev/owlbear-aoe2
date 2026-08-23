fa784b15450f595574c8fcd468c8006e9ebba17e
David Severwright <code@davidsev.co.uk>
2026-08-24 00:01:10 +0100

    fixed webpack not writing manifest correctly

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
                 },
                 {
                     from: 'static/manifest.json',
+                    force: true,
                     transform: (content, path) => {
                         let manifest = JSON.parse(content.toString());
                         manifest.version = version;
